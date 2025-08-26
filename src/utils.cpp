b85bc75e5835376072c363d51de0fb95e5b98970
Hantaro171902 <chaugiahan001@gmail.com>
2025-08-26 12:00:18 +0700

    add utils

---
+++ src/utils.cpp
@@ -0,0 +1,20 @@
+#include "utils.hpp"
+#include <iostream>
+
+using namespace std;
+
+void Utils::clearScreen() {
+    cout << "\033[2J\033[1;1H";
+}
+
+void Utils::moveCursor(int x, int y) {
+    cout << "\033[" << y << ";" << x << "H";
+}
+
+void Utils::hideCursor() {
+    cout << "\033[?25l";
+}
+
+void Utils::showCursor() {
+    cout << "\033[?25h";
+}
